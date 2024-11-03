c65c91607cd43f03cc403b033463d3ee4026eecd
danieLayosh <danielayosh@gmail.com>
2024-11-04 00:25:21 +0200

    added score route to main

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
 from fastapi import FastAPI
-import pandas as pd
-from api.endpoints import scores
+import uvicorn
+from routers.score import router
 
 app = FastAPI()
+
+app.include_router(router)
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the FastAPI application"}
 
 if __name__ == '__main__':
-    # import uvicorn
-    # uvicorn.run(app, host="127.0.0.1", port=8000)
-    # app()
-    scores
+    uvicorn.run(app, host="127.0.0.1", port=8000)
