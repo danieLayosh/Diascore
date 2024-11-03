bc8965ddd1907ef4d7ed4e5b43d09ae64214a981
danieLayosh <danielayosh@gmail.com>
2024-11-03 22:30:43 +0200

    added root route

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+import pandas as pd
+from api.endpoints import scores
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Welcome to the FastAPI application"}
 
 if __name__ == '__main__':
-    app()
+    # import uvicorn
+    # uvicorn.run(app, host="127.0.0.1", port=8000)
+    # app()
+    scores
