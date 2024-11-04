807103cd4e7a2385d9be476fcf47602768b336b8
danieLayosh <danielayosh@gmail.com>
2024-11-05 01:03:45 +0200

    updated read_root message

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app.include_router(router)
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the FastAPI application"}
+    return {"message": "Welcome to the DIASCORE-BASCKEND FastAPI application"}
 
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8000)
