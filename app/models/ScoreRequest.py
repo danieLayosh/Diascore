689ee4543b9df6f358399d3231f82f5f7555f6b0
danieLayosh <danielayosh@gmail.com>
2024-11-11 23:08:11 +0200

    deleted the models i dont need anymore after the merge

--- app/models/ScoreRequest.py
+++
@@ -1,8 +0,0 @@
-from models.AnswerRequest import AnswerSumRuqestWithCred
-
-class ProcessingRequest(AnswerSumRuqestWithCred):
-    pORt: str
-    kORs: str
-    
-class processrequestDone(ProcessingRequest):
-    converted_scores: dict[str, int]
