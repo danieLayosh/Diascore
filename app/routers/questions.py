f921af1af23ae193341366c3570bf3a0b5aed5e6
danieLayosh <danielayosh@gmail.com>
2024-11-11 23:08:44 +0200

    merged all routes to main route that do all the logic

--- app/routers/questions.py
+++ app/routers/questions.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter
-from models.AnswerRequest import AnswerSumRequest, AnswerSumRuqestWithCred
+from models.MainLogicRequest import AnswerSumRequest, AnswerSumRuqestWithCred, processrequestDone
 from services.check_questions_service import check_questions
+from services.excel_service import process_all_scores
 
 router_questions = APIRouter()
 
 @router_questions.post("/questions/sum/")
-async def calculate_answers(request: AnswerSumRequest) -> AnswerSumRuqestWithCred:
+async def calculate_answers(request: AnswerSumRequest) -> processrequestDone:
+    # age validation
+    
+    
     # Extract parameters from the request
     answers = request.answers
     processed_dict = check_questions(answers)
@@ -13,10 +17,13 @@ async def calculate_answers(request: AnswerSumRequest) -> AnswerSumRuqestWithCre
     # Creating the AnswerRequest instance and populating 
     result = AnswerSumRuqestWithCred(
         **request.model_dump(),
-        preprocessed_scores=processed_dict
+        preprocessed_scores=processed_dict, 
+        kORs="kids" if request.age < 6 else "school"
     )
     
-    return result
+    requestDone = process_all_scores(result)
+    
+    return requestDone
 
     
 @router_questions.get("/questions/body/")
