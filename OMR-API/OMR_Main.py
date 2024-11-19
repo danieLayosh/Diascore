17ac34aaaf4d4fc3955dcb275f114bdf88b2251d
danieLayosh <danielayosh@gmail.com>
2024-11-19 22:04:15 +0200

    update OMR_Main.py to change image path, adjust dimensions, and apply perspective transformation

--- OMR-API/OMR_Main.py
+++ OMR-API/OMR_Main.py
@@ -3,9 +3,9 @@ import numpy as np
 import app.services.Utlis as utils
 
 ############################################################################################################
-path = 'OMR-API/app/data/firstPart.png'
-widthImg = int(515 / 1)
-highImg = int(1529 / 1)
+path = 'OMR-API/app/data/first_circles_marked.png'
+widthImg = 389
+hightImg = 1524
 ############################################################################################################
 
 
@@ -13,7 +13,7 @@ img = cv2.imread(path)
 img = utils.resizeAndCropImage(img)
 
 # Preprocessing
-# img = cv2.resize(img, (widthImg, highImg))
+# img = cv2.resize(img, (widthImg, hightImg))
 imgCountours = img.copy()
 imgBiggestCountours = img.copy()
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -27,23 +27,29 @@ cv2.drawContours(imgCountours, contours, -1, (0, 255, 0), 10)
 # Finding Rectangle
 rectCon = utils.rectCounter(contours)
 biggestContour = utils.getConrnerPoints(rectCon[0])
-# secondContour = utils.getConrnerPoints(rectCon[1])
-# print(biggestContour)
 
 if biggestContour.size != 0:
     cv2.drawContours(imgBiggestCountours, biggestContour, -1, (0, 255, 0),20)
-    # cv2.drawContours(imgBiggestCountours, secondContour, -1, (255, 0, 0), 20)
+    
+    biggestContour = utils.reorder(biggestContour)
 
+    pt1 = np.float32(biggestContour)
+    pt2 = np.float32([[0, 0], [widthImg, 0], [0, hightImg], [widthImg, hightImg]])
+    matrix = cv2.getPerspectiveTransform(pt1, pt2)
+    imgWarpColored = cv2.warpPerspective(img, matrix, (widthImg, hightImg))
 
+    # APPLY THRESHOLD
+    imgWarpGray = cv2.cvtColor(imgWarpColored, cv2.COLOR_BGR2GRAY)
+    imgThresh = cv2.threshold(imgWarpGray, 150, 255, cv2.THRESH_BINARY_INV)[1]
 
-imageArray = ([img, imgGray, imgBlur, imgCanny],
-              [imgCountours, imgBiggestCountours,np.zeros_like(img),np.zeros_like(img)])
+# imageArray = ([img, imgGray, imgBlur, imgCanny],
+#               [imgCountours, imgBiggestCountours,imgWarpColored,np.zeros_like(img)])
 
 # imageArray = ([img, imgGray, imgBlur, imgCanny, imgCountours, imgBiggestCountours])
 
-# imageArray = ([imgCountours, imgBiggestCountours,np.zeros_like(img),np.zeros_like(img)])
+imageArray = ([imgCountours, imgBiggestCountours,imgWarpColored,imgThresh])
 
-imgStacked = utils.stackImages(imageArray, 0.2)
+imgStacked = utils.stackImages(imageArray, 0.5)
 
 cv2.imshow('Stacked Images', imgStacked)
 cv2.waitKey(0)
