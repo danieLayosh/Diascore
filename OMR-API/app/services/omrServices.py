7e2f830d3538c8b40d0f8fc9e1bac2d406daf556
danieLayosh <danielayosh@gmail.com>
2024-11-22 15:58:14 +0200

    created preprocess_image to preprocess from image instead of path

--- OMR-API/app/services/omrServices.py
+++ OMR-API/app/services/omrServices.py
@@ -2,9 +2,17 @@ import cv2
 import numpy as np
 import app.services.Utlis as utils
 
-def preprocess_image(path, width, height):
+def preprocess_image_path(path, width, height):
     """Loads and preprocesses the image."""
     img = cv2.imread(path)
+    img = cv2.resize(img, (width, height))
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)
+    imgCanny = cv2.Canny(imgBlur, 10, 50)
+    return img, imgGray, imgBlur, imgCanny
+
+def preprocess_image(img, width, height):
+    """Loads and preprocesses the image."""
     img = cv2.resize(img, (width, height))
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)
@@ -24,6 +32,19 @@ def find_biggest_contour(contours):
         if biggestContour.size != 0:
             return utils.reorder(biggestContour)
     return None
+
+def find_two_biggest_contours(contours):
+    """Finds and returns the two biggest rectangle-like contours."""
+    rectCon = utils.rectCounter(contours)  # Get rectangle-like contours sorted by area
+    biggest_contours = []
+
+    for i in range(min(2, len(rectCon))):  # Process only up to 2 biggest contours
+        contour = utils.getConrnerPoints(rectCon[i])
+        if contour.size != 0:
+            biggest_contours.append(utils.reorder(contour))
+
+    return biggest_contours
+
 
 def warp_perspective(img, biggestContour, width, height):
     """Applies a perspective warp to the image."""
@@ -48,7 +69,7 @@ def process_boxes(imgThresh, num_boxes):
             cv2.countNonZero(boxes[i+2]),
         ]
         counts_sorted = sorted(counts, reverse=True)  # Sort in descending order
-        if counts_sorted[0] - counts_sorted[1] > 2000:  # Check if the difference is > 2000
+        if counts_sorted[0] - counts_sorted[1] > 1000:  # Check if the difference is > 2000
             selected_box = counts.index(counts_sorted[0]) + 1
             reversed_box = 4 - selected_box  # Reverse the mapping
             answers.append(reversed_box)
