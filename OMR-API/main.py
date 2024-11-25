48993234021d18809cd5b54f5b83b8dfddeb4a28
danieLayosh <danielayosh@gmail.com>
2024-11-26 01:03:27 +0200

    add initial test for processing two-page OMR submissions with neat output formatting

--- OMR-API/main.py
+++ OMR-API/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
-import app.services.omr as omr
+from app.services.omr import do_omr_two_pages
+from pprint import pprint  # Importing pprint for neat output formatting
 
 app = FastAPI()
 
@@ -9,5 +10,13 @@ def read_root():
     return {"message": "Welcome to the DIASCORE-OMR FastAPI application"}
 
 if __name__ == '__main__':
-    # uvicorn.run(app, host="127.0.0.1", port=8000)
-    omr.do_omr("OMR-API/app/data/testing_markes_and_angels/IMG_1020.JPG")
+    first_page_path = "OMR-API/app/data/testing_markes_and_angels/IMG_1021.JPG"
+    second_page_base_path = "OMR-API/app/data/testing_markes_and_angels/second_page/IMG_1172.JPG"
+
+    # Initial test with a specific pair of pages
+    result = do_omr_two_pages([
+        first_page_path,
+        second_page_base_path
+    ])
+
+    pprint(result)
