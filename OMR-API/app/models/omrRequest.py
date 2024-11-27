30d4f0fd1e1bcaab906dea9e97efadbde399ea25
danieLayosh <danielayosh@gmail.com>
2024-11-27 18:41:41 +0200

    add OMRResponse model to OMR-API for handling response data

--- OMR-API/app/models/omrRequest.py
+++ OMR-API/app/models/omrRequest.py
@@ -1,4 +1,10 @@
+from fastapi import UploadFile, File
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+from typing import Annotated
 
 class OMRRequest(BaseModel):
     pass
+
+class OMRResponse(BaseModel):
+    answers: dict[int, int]
