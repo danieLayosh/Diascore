1fdb5fb10d2a93e04b0f2daae004df515f6babaf
danieLayosh <danielayosh@gmail.com>
2024-11-28 01:00:12 +0200

    change server port from 8000 to 8001 in main.py

--- OMR-API/app/main.py
+++ OMR-API/app/main.py
@@ -11,4 +11,4 @@ def read_root():
     return {"message": "Welcome to the DIASCORE-OMR FastAPI application"}
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
