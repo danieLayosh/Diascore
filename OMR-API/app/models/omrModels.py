b96cca3c856e29a8b9d7092febea54ac9efd034e
danieLayosh <danielayosh@gmail.com>
2024-11-28 00:32:48 +0200

    remove UploadFile dependency from OMRRequest model and simplify image field definition

--- OMR-API/app/models/omrModels.py
+++ OMR-API/app/models/omrModels.py
@@ -1,9 +1,4 @@
-from fastapi import UploadFile
 from pydantic import BaseModel
-from typing import Dict
-
-class OMRRequest(BaseModel):
-    image: list[UploadFile]
 
 class OMRResponse(BaseModel):
     answers: dict[int, int]
