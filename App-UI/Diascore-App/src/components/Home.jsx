c53878d217a8d23de5f4437f9d1b03a693a80bba
danieLayosh <danielayosh@gmail.com>
2024-12-03 23:22:47 +0200

    add logout functionality to Home component with sign-out button

--- App-UI/Diascore-App/src/components/Home.jsx
+++ App-UI/Diascore-App/src/components/Home.jsx
@@ -1,11 +1,25 @@
-
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase/firebase";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+
+    const navigate = useNavigate();
+
+    const handleSignOut = async () => {
+        try {
+            await signOut(auth);
+            navigate('/');
+        } catch (error) {
+            console.error('Error signing out:', error);
+        }
+    };
 
     return (
         <div>
             <h1>Welcome to the Diascore Home Page!</h1>
             <p>You are successfully logged in.</p>
+            <button onClick={handleSignOut}>Logout</button> {/*Logout button*/}
         </div>
     );
 };
