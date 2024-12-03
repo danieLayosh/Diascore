24bd04e128006b87049e04f9f1d08b4d839b45ea
danieLayosh <danielayosh@gmail.com>
2024-12-03 23:22:32 +0200

    refactor App component to manage user authentication state and redirect routes accordingly

--- App-UI/Diascore-App/src/App.jsx
+++ App-UI/Diascore-App/src/App.jsx
@@ -1,13 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { auth } from './firebase/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
 import Auth from './components/Auth';
 import Home from './components/Home';
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe(); // Cleanup listener on unmount
+  }, []);
+
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Auth />} />
-        <Route path='/home' element={<Home />} />
+        {/* Redirect to home if the user is logged in */}
+        <Route path='/auth' element={user ? <Navigate to='/home' /> : <Auth />} />
+
+        {/* Home page, accessible only if user is logged in */}
+        <Route path='/home' element={user ? <Home /> : <Navigate to="/auth" />} />
+
+        {/* Default route, redirect to auth page */}
+        <Route path='/' element={user ? <Navigate to="/home" /> : <Auth />} />
       </Routes>
     </Router>
   );
