38db76a718fbfd94e5f601e725e27377663a5ac4
danieLayosh <danielayosh@gmail.com>
2024-12-03 22:31:07 +0200

    testing: add Home component for user welcome message after login

---
+++ App-UI/Diascore-App/src/components/Home.jsx
@@ -0,0 +1,13 @@
+
+
+const Home = () => {
+
+    return (
+        <div>
+            <h1>Welcome to the Diascore Home Page!</h1>
+            <p>You are successfully logged in.</p>
+        </div>
+    );
+};
+
+export default Home;
