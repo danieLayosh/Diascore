9911cd505b5fc800ef1babe7597467e08c2d6d2a
danieLayosh <danielayosh@gmail.com>
2024-12-04 19:25:16 +0200

    add ProtectedRoute component to manage access control for authenticated users

---
+++ App-UI/Diascore-App/src/components/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../context/useAuth';
+
+const ProtectedRoute = () => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>; // Show a spinner or loading state
+  }
+
+  return user ? <Outlet /> : <Navigate to="/auth" />;
+};
+
+export default ProtectedRoute;
