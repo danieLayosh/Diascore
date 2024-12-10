4d0782fe906dadc31e481751782482ed115ddbdf
danieLayosh <danielayosh@gmail.com>
2024-12-11 00:54:45 +0200

    refactor App component to remove authentication logic and simplify routing

--- App-UI/Diascore-App/src/App.jsx
+++ App-UI/Diascore-App/src/App.jsx
@@ -1,33 +1,17 @@
-import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { auth } from './firebase/firebase';
-import { onAuthStateChanged } from 'firebase/auth';
 import Auth from './pages/Auth';
 import Home from './pages/Home';
 
 
 const App = () => {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-    });
-
-    return () => unsubscribe(); // Cleanup listener on unmount
-  }, []);
-
+ 
   return (
     <Router>
       <Routes>
-        {/* Redirect to home if the user is logged in */}
-        <Route path='/auth' element={user ? <Navigate to='/home' /> : <Auth />} />
+        <Route path='/' element={<Home />} />
 
-        {/* Home page, accessible only if user is logged in */}
-        <Route path='/home' element={user ? <Home /> : <Navigate to="/auth" />} />
-
-        {/* Default route, redirect to auth page */}
-        <Route path='/' element={user ? <Navigate to="/home" /> : <Auth />} />
+        {/* <Route path='/home' element={user ? <Home /> : <Navigate to="/auth" />} />
+        <Route path='/' element={user ? <Navigate to="/home" /> : <Auth />} /> */}
       </Routes>
     </Router>
   );
