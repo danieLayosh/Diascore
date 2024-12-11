30719c46f7e47deee18ad1dc4510f3978e37a082
danieLayosh <danielayosh@gmail.com>
2024-12-11 13:17:29 +0200

    add descriptive text to Home component highlighting Diascore's features and benefits

--- App-UI/Diascore-App/src/pages/Home.jsx
+++ App-UI/Diascore-App/src/pages/Home.jsx
@@ -12,10 +12,20 @@ const Home = () => {
             <GreenCoverButton text="Sign up" defaultColor="white" />
           </div>
         </div>
-        <div className="home-cover-image">
+        <div >
             <h2>
                 The platform <br /> <span>that will make your life easier</span>
             </h2>
+        </div>
+        <div>
+          <p>
+            Diascore revolutionizes the way you handle diagnostic tests, saving you up to half the time by automating test checks for parents and teachers and calculating scores effortlessly. 
+            <br /><br />
+            You can use Diascore in two convenient ways: send the test online or simply upload a picture of the completed test to the platform. 
+            <br /><br />
+            With Diascore, you will receive a detailed report of test results, providing valuable insights into the students performance.
+          </p>
+
         </div>
 
         <LearnMoreBt />
