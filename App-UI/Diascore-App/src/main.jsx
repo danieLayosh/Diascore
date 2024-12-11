7d79d6889d233c396a84d2c2cec4b3bebabf9ccb
danieLayosh <danielayosh@gmail.com>
2024-12-11 21:45:02 +0200

    Replaced index.css import with ./styles/tailwind.css import

--- App-UI/Diascore-App/src/main.jsx
+++ App-UI/Diascore-App/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import {NextUIProvider} from '@nextui-org/react'
-import './index.css';
+// import './index.css';
+import './styles/tailwind.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './router.jsx';
 import { AuthProvider } from './context/AuthProvider';
