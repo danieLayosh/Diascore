17d18d45f5912b68f178178dc520c9b90081705b
danieLayosh <danielayosh@gmail.com>
2024-12-15 20:25:28 +0200

    Refactor router configuration: update routes to navigate to Home and UserPage components

--- App-UI/Diascore-App/src/router.jsx
+++ App-UI/Diascore-App/src/router.jsx
@@ -2,21 +2,32 @@ import { createBrowserRouter, Navigate } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
 import PublicRoute from './components/PublicRoute'; 
 import appRoutes from './data/routes';
-import Auth from './pages/Auth';
+import Home from './pages/Home';
+import UserPage from './pages/UserPage';
 import PageNotFound from './pages/404Page';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Navigate to='/auth' replace />,
+    element: <Navigate to='/home' replace />,
   },
   {
-    path: '/auth',
-    element: <PublicRoute />, // Use PublicRoute for /auth
+    path: '/home',
+    element: <PublicRoute />, // Use PublicRoute for /home
     children: [
       {
-        path: '/auth',
-        element: <Auth />,
+        path: '/home',
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: '/UserPage',
+    element: <ProtectedRoute />, // Use ProtectedRoute for authenticated routes
+    children: [
+      {
+        path: '/UserPage',
+        element: <UserPage />,
       },
     ],
   },
