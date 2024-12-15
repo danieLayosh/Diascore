af01a95e2c71129e1ced6c7d12e5e8b7f2acaa73
danieLayosh <danielayosh@gmail.com>
2024-12-15 20:25:23 +0200

    Refactor main entry point: enable RouterProvider and remove commented code

--- App-UI/Diascore-App/src/main.jsx
+++ App-UI/Diascore-App/src/main.jsx
@@ -1,18 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import {NextUIProvider} from '@nextui-org/react'
+import { NextUIProvider } from '@nextui-org/react';
 import './styles/tailwind.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './router.jsx';
 import { AuthProvider } from './context/AuthProvider';
-import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <NextUIProvider>
       <AuthProvider>
-        {/* <RouterProvider router={router} /> */}
-        <App/>
+        <RouterProvider router={router} />
       </AuthProvider>
     </NextUIProvider>
   </StrictMode>
