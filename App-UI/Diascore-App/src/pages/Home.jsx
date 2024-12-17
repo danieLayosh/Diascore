b5062f5a1464b8a6ac5ae6c8ff84f06c0844e566
danieLayosh <danielayosh@gmail.com>
2024-12-17 13:03:21 +0200

    Update Home component: adjust heading and paragraph sizes for improved readability and responsiveness

--- App-UI/Diascore-App/src/pages/Home.jsx
+++ App-UI/Diascore-App/src/pages/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
     <div className="flex flex-col items-start w-screen bg-gradient-bg text-text-light min-h-screen ">
       {/* Header Section */}
       <div className="flex justify-between items-center w-full px-4 sm:px-12 lg:px-12 py-4 bg-card-bg shadow-lg">
-        <h3 className="text-5xl sm:text-6xl font-island-moments text-primary-color font-semibold">Diascore</h3>
+        <h3 className="text-4xl sm:text-5xl lg:text-6xl font-island-moments text-primary-color font-semibold">Diascore</h3>
         {/* Login and Signup buttons Section */}
         <div className="flex gap-4">
           <GreenCoverButton text="Log in" defaultColor="black" onClick={handleLoginClick} />
@@ -42,11 +42,11 @@ const Home = () => {
       {/* Intro Section */}
       <div className="mt-16 sm:mt-24 text-left mx-auto sm:mx-auto lg:mx-auto ">
         <h2 className="text-2xl lg:text-6xl sm:text-4xl font-amaranth font-bold leading-tight">
-          The platform <br /> <span className="text-light-primary pl-12 sm:pl-4 lg:pl-48">that will make your life easier!</span>
+          The platform <br /> <span className="text-light-primary pl-2 sm:pl-12 lg:pl-48">that will make your life easier!</span>
         </h2>
       </div>
 
-      <div className="mt-10 sm:mt-16 text-xl sm:text-2xl lg:text-3xl text-left p-5 sm:p-6 lg:p-8 bg-card-bg shadow-lg max-w-5xl  mx-4 sm:mx-8 lg:mx-auto  rounded-3xl">
+      <div className="mt-10 sm:mt-16 text-lg sm:text-2xl lg:text-3xl text-left p-5 sm:p-6 lg:p-8 bg-card-bg shadow-lg max-w-5xl  mx-4 sm:mx-8 lg:mx-auto  rounded-3xl">
         <p className="font-amaranth">
           Diascore revolutionizes the way you handle diagnostic tests, saving you up to half the time by automating test checks for parents and teachers and calculating scores effortlessly.
           <br /><br />
@@ -57,7 +57,7 @@ const Home = () => {
       </div>
 
       {/* Learn More Section Button */}
-      <div className="mt-12 mx-auto">
+      <div className="mt-12 sm:mt-xl lg:mt-2xl mx-auto">
         <LearnMoreBt mode="dark" onClick={handleLearnMoreClick}/>
       </div>
 
