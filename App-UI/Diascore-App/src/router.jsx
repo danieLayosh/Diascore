fe5fca616a8a9c582c92246084e825eb706160ec
danieLayosh <danielayosh@gmail.com>
2024-12-17 12:38:57 +0200

    Remove duplicated routes element from router.jsx

--- App-UI/Diascore-App/src/router.jsx
+++ App-UI/Diascore-App/src/router.jsx
@@ -3,7 +3,6 @@ import ProtectedRoute from './components/ProtectedRoute';
 import PublicRoute from './components/PublicRoute'; 
 import appRoutes from './data/routes';
 import Home from './pages/Home';
-import UserPage from './pages/UserPage';
 import PageNotFound from './pages/404Page';
 
 const router = createBrowserRouter([
@@ -12,22 +11,11 @@ const router = createBrowserRouter([
     element: <Navigate to='/home' replace />,
   },
   {
-    path: '/home',
     element: <PublicRoute />, // Use PublicRoute for /home
     children: [
       {
         path: '/home',
         element: <Home />,
-      },
-    ],
-  },
-  {
-    path: '/UserPage',
-    element: <ProtectedRoute />, // Use ProtectedRoute for authenticated routes
-    children: [
-      {
-        path: '/UserPage',
-        element: <UserPage />,
       },
     ],
   },
