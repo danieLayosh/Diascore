6c655b8c6a54be7696f2ac0fc7cfb7d21a364308
danieLayosh <danielayosh@gmail.com>
2024-12-25 01:36:29 +0200

    Wrap RouterProvider with AlertProvider and include GlobalAlert component

--- App-UI/Diascore-App/src/main.jsx
+++ App-UI/Diascore-App/src/main.jsx
@@ -5,12 +5,17 @@ import './styles/tailwind.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './router.jsx';
 import { AuthProvider } from './context/AuthProvider';
+import { AlertProvider } from './context/AlertContext.jsx';
+import GlobalAlert from './components/alerts/GlobalAlert';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <NextUIProvider>
       <AuthProvider>
-        <RouterProvider router={router} />
+        <AlertProvider>
+          <GlobalAlert />
+          <RouterProvider router={router} />
+        </AlertProvider>
       </AuthProvider>
     </NextUIProvider>
   </StrictMode>
