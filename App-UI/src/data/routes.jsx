c5ce389d85771d81a17e7bb385b28e97e7f8b716
danieLayosh <danielayosh@gmail.com>
2025-02-09 22:02:51 +0200

    Add route for NewDiagnosis component

--- App-UI/src/data/routes.jsx
+++ App-UI/src/data/routes.jsx
@@ -1,9 +1,14 @@
 import Home from "../pages/Home";
+import NewDiagnosis from "../pages/Diagnosis/NewDiagnosis";
 
 const appRoutes = [
     {
         path: '/Home',
         element: <Home /> 
+    },
+    {
+        path: '/diagnosis/new',
+        element: <NewDiagnosis />
     }
 ]
 
