3074bfb745e8478dbdab1b931ccc8af8a09f14b1
danieLayosh <danielayosh@gmail.com>
2025-02-09 22:28:02 +0200

    Add NewDiagnosisForm component with basic layout

---
+++ App-UI/src/components/forms/NewDiagnosisForm.jsx
@@ -0,0 +1,11 @@
+
+const NewDiagnosisForm = () => {
+
+    return (
+        <div className="flex flex-col text-center w-screen bg-gradient-bg text-text-light min-h-screen">
+            <h1>New Diagnosis</h1>
+        </div>
+    );
+};
+
+export default NewDiagnosisForm;
