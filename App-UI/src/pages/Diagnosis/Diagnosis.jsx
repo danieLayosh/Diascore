a4944799ca0c102baba6d1a9409d900f1401bbe5
danieLayosh <danielayosh@gmail.com>
2025-02-11 23:11:41 +0200

    Add UUID prop and fetch diagnostic data in Diagnosis component

--- App-UI/src/pages/Diagnosis/Diagnosis.jsx
+++ App-UI/src/pages/Diagnosis/Diagnosis.jsx
@@ -1,12 +1,24 @@
+import PropTypes from 'prop-types';
+import { getDiagnosticDataByUUID } from '../../firebase/firestore/diagnoses';
 
+const Diagnosis = ({uuid}) => {
+    const data = getDiagnosticDataByUUID(uuid);
 
-const Diagnosis = () => {
+    if (!data) {
+        return <div>Loading...</div>;
+    } else print(data);
 
     return (
         <div>
             <h1>Diagnosis</h1>
+            <p>UUID: {uuid}</p>
+            <p>Data: {data}</p>
         </div>
     );
+};
+
+Diagnosis.propTypes = {
+    uuid: PropTypes.string.isRequired,
 };
 
 export default Diagnosis;
